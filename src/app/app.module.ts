4488f9c14437037cc3a07c18d67ca8f197ed0307
chadwick <desarrollos.emmanueldavilad@gmail.com>
2020-01-26 00:39:59 +0100

    code

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { FotosComponent } from './components/fotos/fotos.component';
+import { CargaComponent } from './components/carga/carga.component';
+
+import { APP_ROUTES } from './app.routes';
+import { CargaImagenesService } from './services/carga-imagenes.service';
+
+// Firebase
+
+
+import { environment } from '../environments/environment';
+import { NgDropFilesDirective } from './directives/ng-drop-files.directive';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import * as firebase from 'firebase'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FotosComponent,
+    CargaComponent,
+    NgDropFilesDirective
+  ],
+  imports: [
+    BrowserModule,
+    APP_ROUTES,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule
+  ],
+  providers: [
+    CargaImagenesService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
